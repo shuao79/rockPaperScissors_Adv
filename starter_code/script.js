a66e6552e054d7ce994ba72d96bd1f50f1706cc9
shuao79 <tahirtanzimkhan@mybihs.org>
2015-10-29 21:58:51 +0000

    example comment

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,13 +3,27 @@ var aiPoint = 0;
 
 // This function returns the selection of the computer
 function getAISelection() {
-    //TODO: randomly choose between 'rock', 'paper', or 'scissors'
+    Math.random()
+    
+    return "random";
 }
 
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
-    //TODO: pick the correct winner: user or ai
-    //TODO: Add one point for the winner
+    if (userValue===aiValue)
+    return "draw"
+    else if (userValue== "paper" && aiValue== "rock")
+       return "user" ;
+    else if (userValue== "rock" && aiValue== "scissors")
+       return "user" ;
+    else if (userValue== "scissors" && aiValue== "paper")
+       return "user" ;
+     else if (userValue== "rock" && aiValue== "paper")
+       return "ai" ;
+    else if (userValue== "scissors" && aiValue== "rock")
+       return "ai" ;
+    else if (userValue== "paper" && aiValue== "scissors") 
+       return "ai" ;
 }
 
 // This function sets the scoreboard with the correct points
@@ -27,7 +41,7 @@ function evaluate(evt) {
     if ( 'user' === winner ) {
         $('#message').delay(50).text('You have won!, Click a box to play again');
     } else if ( winner === 'draw' ) {
-        $('#message').delay(50).text('Draw! Click a box to play again');
+       $('#message').delay(50).text('Draw! Click a box to play again');
     } else {
         $('#message').delay(50).text('You have lost!, Click a box to play again');
     }
@@ -35,5 +49,6 @@ function evaluate(evt) {
 
 // This function runs on page load
 $(document).ready(function(){
-
+    $(".token").click(evaluate);
+    
 });

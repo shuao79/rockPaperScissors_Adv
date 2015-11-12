3da221fb981484a6818c94e5adcebdc7ac285b3f
shuao79 <tahirtanzimkhan@mybihs.org>
2015-11-12 21:05:57 +0000

    changed setscore

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,32 +3,39 @@ var aiPoint = 0;
 
 // This function returns the selection of the computer
 function getAISelection() {
-    Math.random()
-    
-    return "random";
+    var random_math = Math.floor(Math.random() *3 );
+    if (random_math === 0 ) { 
+        return "rock";
+    } else if (random_math === 1 ) {
+       return "paper"
+    } 
+    else if (random_math === 2 ) {
+       return "scissors"
+    }  
 }
 
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
     if (userValue===aiValue)
     return "draw"
-    else if (userValue== "paper" && aiValue== "rock")
+    else if (userValue=== "paper" && aiValue=== "rock")
        return "user" ;
-    else if (userValue== "rock" && aiValue== "scissors")
+    else if (userValue=== "rock" && aiValue=== "scissors")
        return "user" ;
-    else if (userValue== "scissors" && aiValue== "paper")
+    else if (userValue=== "scissors" && aiValue=== "paper")
        return "user" ;
-     else if (userValue== "rock" && aiValue== "paper")
+     else if (userValue=== "rock" && aiValue=== "paper")
        return "ai" ;
-    else if (userValue== "scissors" && aiValue== "rock")
+    else if (userValue=== "scissors" && aiValue=== "rock")
        return "ai" ;
-    else if (userValue== "paper" && aiValue== "scissors") 
+    else if (userValue=== "paper" && aiValue=== "scissors") 
        return "ai" ;
 }
 
 // This function sets the scoreboard with the correct points
-function setScore() {
-
+var setScore = function setScore() {
+   $('#aiPoint').text(aiPoint);
+   $('#userPoint').text(userPoint);
 }
 
 // This function captures the click and picks the winner
@@ -40,10 +47,13 @@ function evaluate(evt) {
 
     if ( 'user' === winner ) {
         $('#message').delay(50).text('You have won!, Click a box to play again');
+        userPoint ++;
     } else if ( winner === 'draw' ) {
        $('#message').delay(50).text('Draw! Click a box to play again');
     } else {
+       aiPoint ++;
         $('#message').delay(50).text('You have lost!, Click a box to play again');
+        
     }
 }
 
